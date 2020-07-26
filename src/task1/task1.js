379ad7cac195d2cb858af363b8681e34b1224e4b
Dzianis Davydau <dzianis_davydau@epam.com>
2020-07-26 17:23:59 +0300

    feat: implemented task 1.1 (HomeTask1)

---
+++ src/task1/task1.js
@@ -0,0 +1,24 @@
+const { promises } = require('fs');
+
+const inputDirPath = './src/task1/stdin';
+const outputDirPath = './src/task1/stdout';
+
+function reverseContentStrings(stringContent) {
+    const lines = stringContent.split(/\r?\n/);
+    const reversedLines = lines.map((line) => line.split('').reverse().join(''));
+    return reversedLines.join('\r\n');
+}
+
+async function readInputAsync(path) {
+    const fileContent = await promises.readFile(path, { encoding: 'ascii' });
+    return fileContent;
+}
+
+async function writeOutputAsync(path, content) {
+    const reversedContent = reverseContentStrings(content);
+    return await promises.writeFile(path, reversedContent);
+}
+
+readInputAsync(inputDirPath)
+    .then((content) => writeOutputAsync(outputDirPath, content))
+    .catch(({ message }) => console.log(`Error: ${message}`));
