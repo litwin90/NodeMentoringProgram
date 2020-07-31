2fac1f31866c378986c2043b751c970df2946c45
Dzianis Davydau <dzianis_davydau@epam.com>
2020-07-31 23:20:26 +0300

    fix: used pipeline method for task 1.2 (HomeTask1)

--- src/task1/task1.js
+++ src/task1/task1.js
@@ -1,24 +1,17 @@
-import { promises } from 'fs';
+const readline = require('readline');
 
-const inputDirPath = './src/task1/stdin';
-const outputDirPath = './src/task1/stdout';
-
-function reverseContentStrings(stringContent) {
-    const lines = stringContent.split(/\r?\n/);
-    const reversedLines = lines.map((line) => line.split('').reverse().join(''));
-    return reversedLines.join('\r\n');
+function reverseString(string) {
+    return string.split('').reverse().join('');
 }
 
-async function readInputAsync(path) {
-    const fileContent = await promises.readFile(path, { encoding: 'ascii' });
-    return fileContent;
-}
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+});
 
-async function writeOutputAsync(path, content) {
-    const reversedContent = reverseContentStrings(content);
-    return await promises.writeFile(path, reversedContent);
-}
-
-readInputAsync(inputDirPath)
-    .then((content) => writeOutputAsync(outputDirPath, content))
-    .catch(({ message }) => console.log(`Error: ${message}`));
+rl.on('line', (input) => {
+    if (input) {
+        console.log(reverseString(input));
+    }
+});
--- src/task2/task2.js
+++ src/task2/task2.js
@@ -1,14 +1,31 @@
 import csv from 'csvtojson';
+import { pipeline } from 'stream';
 import { createReadStream, createWriteStream } from 'fs';
 
-const csvFilePath = './src/task2/csv/csvInput.csv';
+const csvFilePath = './csv/csvInput.csv';
 const txtOutputFilePath = './src/task2/output.txt';
 
 const readStream = createReadStream(csvFilePath);
+const chunkedStream = csv({
+    ignoreColumns: /(Amount)/,
+})
+    .preFileLine((fileLineString, lineIdx) => {
+        const isHeaderLine = lineIdx === 0;
+        if (isHeaderLine) {
+            return fileLineString.toLowerCase();
+        }
+        return fileLineString;
+    })
+    .fromStream(readStream);
+
 const writeStream = createWriteStream(txtOutputFilePath);
 
-csv()
-    .fromStream(readStream)
-    .on('data', (data) => console.log(`READ_STREAM_DATA_CHUNK: ${data}`))
-    .pipe(writeStream)
-    .on('error', (error) => console.log(`ERROR: ${error.message}`));
+const finalize = (error) => {
+    if (error) {
+        console.log(`ERROR: ${error.message}`);
+    } else {
+        console.log('PIPELINE SUCCEEDED.');
+    }
+};
+
+pipeline(chunkedStream, writeStream, finalize);
